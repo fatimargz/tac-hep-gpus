62b2e9b7235fb77d39444229baa1a613f10e8708
Fatima Rodriguez <frodriguez@g38n01.hep.wisc.edu>
2024-10-06 21:01:23 -0500

    completed

--- week3/swap_vectors.cu
+++ week3/swap_vectors.cu
@@ -6,43 +6,62 @@ const int block_size = 256;
 const int grid_size = DSIZE/block_size;
 
 
-__global__ void vector_addition(FIXME) {
+__global__ void swap_vectors(float *A, float *B, int v_size) {
 
     //FIXME:
     // Express the vector index in terms of threads and blocks
-    int idx =  ;
+    int idx =  threadIdx.x + blockDim.x * blockIdx.x;
     // Swap the vector elements - make sure you are not out of range
-
+    if (idx < v_size){
+	    float a = A[idx];
+	    A[idx] = B[idx];
+	    B[idx] = a;
+    }
 }
 
 
 int main() {
 
 
-    float *h_A, *h_B, *h_C, *d_A, *d_B, *d_C;
+    float *h_A, *h_B, *d_A, *d_B;
     h_A = new float[DSIZE];
     h_B = new float[DSIZE];
-    h_C = new float[DSIZE];
 
 
     for (int i = 0; i < DSIZE; i++) {
         h_A[i] = rand()/(float)RAND_MAX;
         h_B[i] = rand()/(float)RAND_MAX;
-        h_C[i] = 0;
     }
+    // Print old vectors
+    printf("before: A[0] = %f\n", h_A[0]);
+    printf("before: B[0] = %f\n", h_B[0]);
+
 
 
     // Allocate memory for host and device pointers 
-
+    cudaMalloc(&d_A, DSIZE*sizeof(float));
+    cudaMalloc(&d_B, DSIZE*sizeof(float));
+    
     // Copy from host to device
-
+    cudaMemcpy(d_A, h_A, DSIZE*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, DSIZE*sizeof(float), cudaMemcpyHostToDevice);
     // Launch the kernel
+    swap_vectors<<<grid_size, block_size>>>(d_A, d_B, DSIZE);
 
     // Copy back to host 
+    cudaMemcpy(h_A, d_A, DSIZE*sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_B, d_B, DSIZE*sizeof(float), cudaMemcpyDeviceToHost);
 
     // Print and check some elements to make sure swapping was successfull
+    printf("after: A[0] = %f\n", h_A[0]);
+    printf("after: B[0] = %f\n", h_B[0]);
+
 
     // Free the memory 
-
-    return 0;
+    free(h_A);
+    free(h_B);
+ 
+    cudaFree(d_A);
+    cudaFree(d_B);
+    
 }
