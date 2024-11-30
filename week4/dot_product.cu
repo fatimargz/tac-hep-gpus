7cb3a0eb7afca364095b5a01e9f63082f14a5f04
Fatima Rodriguez <frodriguez@login06.hep.wisc.edu>
2024-11-29 19:35:16 -0600

    finally committed

--- week4/dot_product.cu
+++ week4/dot_product.cu
@@ -24,8 +24,11 @@ const int b = 1;
 // CUDA kernel that runs on the GPU
 __global__ void dot_product(const int *A, const int *B, int *C, int N) {
 
-	// FIXME
-	// Use atomicAdd	
+	int idx = threadIdx.x + blockDim.x * blockIdx.x;
+        if (idx < N){
+                atomicAdd(C,A[i] * B[i]);
+        }
+
 }
 
 
@@ -47,23 +50,45 @@ int main() {
 
 
 	// Allocate device memory 
-	
+	cudaMalloc(&d_A, sizeof(int)*DSIZE);
+        cudaMalloc(&d_B,sizeof(int)*DSIZE);
+        cudaMalloc($d_C,sizeof(int));
+
 	// Check memory allocation for errors
+	cudaCheckErrors();
 
 	// Copy the matrices on GPU
-	
+	cudaMemcpy(&d_A, sizeof(int)*DSIZE, cudaMemcpyHostToDevice);
+        cudaMemcpy(&d_B, sizeof(int)*DSIZE, cudaMemcpyHostToDevice);
+        cudaMemcpy(&d_C, sizeof(int), cudaMemcpyHostToDevice);
+
 	// Check memory copy for errors
+	cudaCheckErrors();
 
 	// Define block/grid dimentions and launch kernel
-	
+	dim3 blockSize(BLOCK_SIZE);  // 32 threads in x, 16 threads in y
+        dim3 gridSize((DSIZE+BLOCK_SIZE-1)/BLOCK_SIZE);
+	dot_product<<<grid_size,block_size>>>(d_A, d_B, d_C, DSIZE);
+
 	// Copy results back to host
-	
+	 cudaMemcpy(h_C,d_C, sizeof(int), cudaMemcpyDeviceToHost);
+
     // Check copy for errors
+	 cudaCheckErrors();
 
 	// Verify result
+	std::cout<<*h_A,*h_B,*h_C<<std::endl;
 
 	// Free allocated memory
-	
+	free(h_A);
+        free(h_B);
+        free(h_C);
+
+        cudaFree(d_A);
+        cudaFree(d_B);
+        cudaFree(d_C);
+
+
 	return 0;
 
 }
